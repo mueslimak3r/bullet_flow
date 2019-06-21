3799c2430d73e5c450c7b9efa6ab68c2161a9aa6
Alexander Kozma <alexanders.kozma@gmail.com>
2019-06-21 16:53:48 -0700

    player controller updates

--- game/player.gd
+++ game/player.gd
@@ -6,25 +6,44 @@ extends Node2D
 
 var anim = "default"
 var flip = 0
+var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _input(event):
-	if (event.is_action_pressed("move_left")):
+func get_input():
+	velocity = Vector2()
+	if (Input.is_action_pressed("move_left")):
 		flip = 1
 	else:
 		flip = 0
-	if (event.is_action_pressed("move_up")):
+	if (Input.is_action_pressed("move_up")):
 		anim = "run_up"
-		
-	if (event.is_action_pressed("move_down")):
+		velocity.y -= 1
+	if (Input.is_action_pressed("move_down")):
 		anim = "run_down"
-	if (event.is_action_pressed("move_right")) or (event.is_action_pressed("move_left")):
+		velocity.y += 1
+	if (Input.is_action_pressed("move_right")) or (Input.is_action_pressed("move_left")):
 		anim = "run_side"
+	if (Input.is_action_pressed("move_right")):
+		velocity.x += 1
+	if (Input.is_action_pressed("move_left")):
+		velocity.x -= 1
+	velocity = velocity.normalized() * 200
+	pass
+
+func _input(event):
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$AnimatedSprite.play(anim)
+	get_input()
+	if (flip):
+		$KinematicBody2D/AnimatedSprite.flip_h = true
+	else:
+		$KinematicBody2D/AnimatedSprite.flip_h = false
+	if (velocity.x == 0 && velocity.y == 0):
+		anim = "default"
+	$KinematicBody2D/AnimatedSprite.play(anim)
+	velocity = $KinematicBody2D.move_and_slide(velocity)
