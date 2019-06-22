5b1a341149b3f52da5a6055dcb78dcd67dcaf5c8
Cameron <cameronlambert98@gmail.com>
2019-06-21 20:51:27 -0700

    resized flying demon and added script

--- game/scenes/characters/flying_demon.gd
+++ game/scenes/characters/flying_demon.gd
@@ -6,8 +6,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$KinematicBody2D/AnimatedSprite.play("idle")
 
+func seek_player() :
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
