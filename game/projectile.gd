2bafb0bdc1d3ae95083d0ffc4322b95aa550f0be
Alexander Kozma <alexanders.kozma@gmail.com>
2019-06-23 01:59:34 -0700

    bullet collision fix

--- game/projectile.gd
+++ game/projectile.gd
@@ -26,12 +26,33 @@ func _process(delta):
 	lastpos = $KinematicBody2D.global_position
 
 func _on_Area2D_body_entered(body):
+	if (body.get_parent()):
+		if (body.get_parent().get_parent()):
+			if "health" in body.get_parent().get_parent():
+				body.get_parent().get_parent().health -= damage
+	print(body.get_parent().get_name())
+	if ("ghost" in body.get_parent()):
+		print("Y")
+		if body.get_parent().ghost == 1:
+			return
 	if !("player" in body.get_parent()):
+		print("HIT")
 		queue_free()
 
 func _on_Area2D_area_entered(area):
+	if (area.get_parent()):
+		if (area.get_parent().get_parent()):
+			if "health" in area.get_parent().get_parent():
+				area.get_parent().get_parent().health -= damage
+	print(area.get_parent().get_name())
+	if ("ghost" in area.get_parent()):
+		print("X")
+		if area.get_parent().ghost == 1:
+			return
 	print("HELLO")
-	queue_free()
+	if !("player" in area.get_parent()):
+		print("HITAREA")
+		queue_free()
 
 
 func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
